170264aee0280756c78bc77953e35c1957d520fb
Rexpeto <Carlosgallardoparra@hotmail.com>
2022-12-31 10:42:40 -0400

    rutas

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get('/inicio', (req, res) => res.send('inicio'));
+router.get('/', (req, res) => res.render('home'));
 
 router.get('/register', (req, res) => res.send('Crear cuenta'));
 
