56213f26a6bd7c782ec24cb3e5909c7b9ffda84c
Rexpeto <Carlosgallardoparra@hotmail.com>
2022-12-31 10:07:21 -0400

    Utilización de rutas

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from "express";
 import dotenv from 'dotenv';
+import rutas from "./routes/index.js";
 
+//? Creando app
 const app = express();
 
+//? Configuración de dotenv
 dotenv.config({path: '.env'});
+
+//? Utilización de rutas
+app.use('/', rutas);
+
+//? utilizando el puerto
 const port = process.env.PORT || 4000;
+
 app.listen(port, () => console.log(`Funcionando en el puerto ${port} 🔥🔥🔥`));
