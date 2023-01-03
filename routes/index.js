abe494bf2370eb518ea6f38af25d13123ec3b1ea
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-01-03 01:40:13 -0400

    Ruta para crear usuarios

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { inicio } from "../controllers/homeController.js";
-import { login, register } from "../controllers/authController.js";
+import { crearUsuario, login, register } from "../controllers/authController.js";
 
 const router = Router();
 
@@ -9,5 +9,8 @@ router.get('/', inicio);
 router.get('/register', register);
 
 router.get('/login', login);
+
+//? Crea un nuevo usuario
+router.post('/register', crearUsuario);
 
 export default router;
