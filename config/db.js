e9001ff8bf6db2b6334b6b4a0bd626f7ec980a52
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-01-03 01:41:46 -0400

    Actualizado

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config({path: '.env'});
 
-export const db = new sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASS, {
+const db = new sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'postgres',
@@ -12,5 +12,8 @@ export const db = new sequelize(process.env.DB, process.env.DB_USER, process.env
         min: 0,
         acquire: 30000,
         idle: 10000
-    }
+    },
+    logging: false
 });
+
+export default db;
