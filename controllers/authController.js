6f3169311f1f65ac2e2e1fa01c0ba4618ada138d
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-01-03 01:41:13 -0400

    Controlador para crear nuevos usuarios

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import Usuarios from '../models/Usuarios.js'
 
 export const register = (req, res) => {
     res.render('register', {
@@ -10,4 +11,14 @@ export const login = (req, res) => {
     res.render('login', {
         pagina: 'Iniciar sesión'
     })
+}
+
+//? Crea un nuevo usuario en la db
+export const crearUsuario = async (req, res) => {
+    const user = req.body;
+
+    const usuario = await Usuarios.create(user);
+
+    //TODO: Flash Message y redireccionar 
+    console.log(`Usuario creado correctamente:`, usuario);
 }
