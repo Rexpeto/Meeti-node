93192f43ee50f51050b30e1d87a01c898e55479d
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-01-05 15:45:56 -0400

    Ruta para confirmar correos

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { body } from "express-validator";
 import { inicio } from "../controllers/homeController.js";
-import { crearUsuario, login, register } from "../controllers/authController.js";
+import { confirmarC, crearUsuario, login, register } from "../controllers/authController.js";
 
 const router = Router();
 
@@ -15,5 +15,8 @@ router.get('/login', login);
 router.post('/register',
 body('password_r').notEmpty().withMessage('Repetir contraseña no puede estar vació'),
 crearUsuario);
+
+//? Confirma un usuario
+router.get('/confirmar-cuenta/:email', confirmarC);
 
 export default router;
