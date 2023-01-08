842d3dcb5567316cba67f601932e098e2a635a95
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-01-08 11:34:03 -0400

    Utilización de modelo de categorias

--- controllers/gruposControllers.js
+++ controllers/gruposControllers.js
@@ -1,5 +1,10 @@
-export const nuevoGrupo = (req, res) => {
+import Categorias from "../models/Categorias.js";
+
+export const nuevoGrupo = async (req, res) => {
+    const categorias = await Categorias.findAll();
+    
     res.render('nuevo-grupos', {
-        pagina: 'Crea nuevo grupo'
+        pagina: 'Crea nuevo grupo',
+        categorias
     });
 }
