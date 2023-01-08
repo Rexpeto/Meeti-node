f4e158b140c85a2b06d5d6e5e9cc16cee82c72fe
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-01-08 12:58:44 -0400

    guardarGrupo

--- controllers/gruposControllers.js
+++ controllers/gruposControllers.js
@@ -1,4 +1,5 @@
 import Categorias from "../models/Categorias.js";
+import Grupos from "../models/Grupos.js";
 
 export const nuevoGrupo = async (req, res) => {
     const categorias = await Categorias.findAll();
@@ -7,4 +8,21 @@ export const nuevoGrupo = async (req, res) => {
         pagina: 'Crea nuevo grupo',
         categorias
     });
+}
+
+//? Guarda los grupos
+export const guardarGrupo = async (req, res) => {
+    const grupo = req.body;
+
+    try {
+        //* Almacena en la bd
+        const nuevo = await Grupos.create(grupo);
+
+        req.flash('exito', 'Se ha creado un grupo con exito');
+        res.redirect('/administracion');
+    } catch (error) {
+        console.log(error);
+        req.flash('error', error);
+        res.redirect('/nuevo-grupo');
+    }
 }
