d253ad7a0fd9436d8d8458b39b8cfe691237716c
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-01-15 23:04:12 -0400

    Mostrar los grupos del usuario

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,12 @@
-export const adminPanel = (req, res) => {
+import Grupos from "../models/Grupos.js"
+
+export const adminPanel = async (req, res) => {
+    //* Consultar grupos el usuario
+    const grupos = await Grupos.findAll({where: { UsuarioId: req.user.id }});
+
+
     res.render('adminPanel', {
-        pagina: 'Panel de administración'
-    })
+        pagina: 'Panel de administración',
+        grupos
+    });
 }
