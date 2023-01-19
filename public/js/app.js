4cfe1f3cffdad5aaa9ffb9afa44dfb1b1ea5a54f
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-01-18 23:04:54 -0400

    leaflet con webpack

---
+++ public/js/app.js
@@ -0,0 +1,27 @@
+import { OpenStreetMapProvider } from "leaflet-geosearch";
+
+const lat = 10.60003150010103;
+const lng = -71.64964139210007;
+const mapa = L.map('mapa').setView([lat, lng ], 13);
+
+document.addEventListener('DOMContentLoaded', () => {
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mapa);
+
+    //* Seleccionar el buscador
+    const buscador = document.querySelector('#formbuscador');
+
+    buscador.addEventListener('input', buscarDireccion);
+});
+
+const buscarDireccion = e => {
+    if(e.target.value.length > 5) {
+        //* Utilizar el provider
+        const provider = new OpenStreetMapProvider();
+
+        provider.search({query: e.target.value}).then(result => {
+            console.log(result)
+        });
+    }
+}
